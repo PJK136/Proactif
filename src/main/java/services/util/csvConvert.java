20028761eaacd2724906c417ecd100eec538870c
tcadet <azergante@gmail.com>
2018-03-17 17:37:13 +0100

    ajout de la méthode loadClient
    permet de charger des clients à partir d'un fichier tsv

--- src/main/java/services/util/csvConvert.java
+++ src/main/java/services/util/csvConvert.java
@@ -1,9 +1,12 @@
 package services.util;
 
 import entities.Address;
+import entities.Client;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
@@ -24,11 +27,31 @@ public class csvConvert {
     
     public static List<Employee> loadEmployee(String employeeFile) {
         
-    }
+    }*/
     
     public static List<Client> loadClient(String clientFile) {
-        
-    }*/
+        List<Client> clients = new LinkedList<>();   
+        Iterable<CSVRecord> records = null;
+        try {
+            records = CSVParser.parse(Paths.get(clientFile), StandardCharsets.UTF_8, CSVFormat.TDF.withFirstRecordAsHeader());
+        } catch (IOException ex) {
+            Logger.getLogger(csvConvert.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        for (CSVRecord record : records) {
+            Client client = new Client(
+                record.get("honorific"),
+                record.get("firstName"),
+                record.get("lastName"),
+                dateFormat.parse(record.get("birthdate"), new ParsePosition(0)),
+                record.get("phoneNumber"),
+                record.get("email"),
+                null
+            );
+            clients.add(client);                    
+        }
+        return clients; 
+    }
     
     public static List<Address> loadAddress(String addressFile) {        
         List<Address> addresses = new LinkedList<>();   
@@ -59,14 +82,21 @@ public class csvConvert {
     
     public static void main(String[] args) {
         final String dataPath = "src/main/resources/mockData/";
-        System.out.println("------EMPLOYEE ADRESSES------");
+       
+        /*System.out.println("------EMPLOYEE ADRESSES------");
         for(Address address: loadAddress(dataPath + "employee_address.csv")) {
             System.out.println(address);
         }
         System.out.println("------CLIENT ADRESSES------");
         for(Address address: loadAddress(dataPath + "client_address.csv")) {
             System.out.println(address);
+        }*/
+        
+        System.out.println("------CLIENTS------");
+        for(Client client: loadClient(dataPath + "client.csv")) {
+            System.out.println(client);
         }
+        
         System.out.println("------END------");
     }
 }
