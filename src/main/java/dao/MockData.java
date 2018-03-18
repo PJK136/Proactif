4306948db6bb0512a590286129634ad038f73e73
Paul Du <du.paul136@gmail.com>
2018-03-19 00:19:27 +0100

    Factorisation MockData Quand même, l'héritage et le polymorphisme, ça sert à quelque chose !

--- src/main/java/dao/MockData.java
+++ src/main/java/dao/MockData.java
@@ -1,7 +1,6 @@
 package dao;
 
-import entities.Client;
-import entities.Employee;
+import entities.Person;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
@@ -17,18 +16,24 @@ public class MockData {
    final static String DATA_PATH = "src/main/resources/mockData/";
    final static int FLUSH_THRESHOLD = 1000;
    
-   public static boolean[] insertAll() {
-       return new boolean[] {insertEmployees(), insertClients()};
-   }
+    public static boolean[] insertAll() {
+        return new boolean[] {insertEmployees(), insertClients()};
+    }
+      
+    public static boolean insertEmployees() {
+        return insertPersons(CsvConvert.loadEmployeesWithAddresses(Paths.get(DATA_PATH + "employee.csv"), Paths.get(DATA_PATH + "employee_address.csv")));
+    } 
    
-   public static boolean insertEmployees() {
+    public static boolean insertClients() {
+        return insertPersons(CsvConvert.loadClientsWithAddresses(Paths.get(DATA_PATH + "client.csv"), Paths.get(DATA_PATH + "client_address.csv")));
+    }
+     
+    public static boolean insertPersons(List<? extends Person> persons) {
         JpaUtil.createEntityManager();
         EntityManager em = JpaUtil.getEntityManager();
-        List<Employee> employees = CsvConvert.loadEmployeesWithAddresses(Paths.get(DATA_PATH + "employee.csv"), Paths.get(DATA_PATH + "employee_address.csv"));
-   
         JpaUtil.beginTransaction();
-        for (int i = 0; i < employees.size(); i++) {
-            em.persist(employees.get(i));
+        for (int i = 0; i < persons.size(); i++) {
+            PersonDAO.create(persons.get(i));
             if ((i % FLUSH_THRESHOLD) == 0) {
                 em.flush();
                 em.clear();
@@ -44,34 +49,9 @@ public class MockData {
             JpaUtil.closeEntityManager();
         }
         return true;
-   } 
+    }
    
-   public static boolean insertClients() {
-        JpaUtil.createEntityManager();
-        EntityManager em = JpaUtil.getEntityManager();
-        List<Client> clients = CsvConvert.loadClientsWithAddresses(Paths.get(DATA_PATH + "client.csv"), Paths.get(DATA_PATH + "client_address.csv"));
-   
-        JpaUtil.beginTransaction();
-        for (int i = 0; i < clients.size(); i++) {
-            em.persist(clients.get(i));
-            if ((i % FLUSH_THRESHOLD) == 0) {
-                em.flush();
-                em.clear();
-            }
-        }
-        
-        try {
-            JpaUtil.commitTransaction();
-        } catch(RollbackException e) {
-            JpaUtil.rollbackTransaction(); 
-            return false;
-        } finally {
-            JpaUtil.closeEntityManager();
-        }
-        return true;
-   }
-   
-   public static void main(String[] args) {
+    public static void main(String[] args) {
        JpaUtil.init();
        logger.info("-----MOCK DATA START-----");
        
