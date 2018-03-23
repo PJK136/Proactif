b13eb8c7d50ab8ae52a22e30189f4441dd84f3fa
Paul Du <du.paul136@gmail.com>
2018-03-23 02:52:21 +0100

    Trie les interventions par date

--- src/main/java/dao/InterventionDAO.java
+++ src/main/java/dao/InterventionDAO.java
@@ -24,26 +24,26 @@ public class InterventionDAO {
     }
     
     public static List<Intervention> findInterventionsByClient(Long clientId) {
-        Query query = JpaUtil.getEntityManager().createQuery("SELECT i FROM Intervention i WHERE i.client.id = :id")
+        Query query = JpaUtil.getEntityManager().createQuery("SELECT i FROM Intervention i WHERE i.client.id = :id ORDER BY i.startDate")
         .setParameter("id", clientId);
         return (List<Intervention>) query.getResultList();
     }
 
     public static Intervention findInterventionToDoByEmployee(Long employeeId) {
-        Query query = JpaUtil.getEntityManager().createQuery("SELECT i FROM Intervention i WHERE i.endDate = NULL AND i.employee.id = :id")
+        Query query = JpaUtil.getEntityManager().createQuery("SELECT i FROM Intervention i WHERE i.endDate = NULL AND i.employee.id = :id ORDER BY i.startDate")
         .setParameter("id", employeeId);
         List<Intervention> interventions = (List<Intervention>) query.getResultList();
         return interventions.isEmpty()?null:interventions.get(0);
     }
 
     public static List<Intervention> findFinishedInterventionsByEmployee(Long employeeId) {
-        Query query = JpaUtil.getEntityManager().createQuery("SELECT i FROM Intervention i WHERE i.endDate != NULL AND i.employee.id = :employeeId")
+        Query query = JpaUtil.getEntityManager().createQuery("SELECT i FROM Intervention i WHERE i.endDate != NULL AND i.employee.id = :employeeId ORDER BY i.startDate")
                 .setParameter("employeeId", employeeId);
         return (List<Intervention>) query.getResultList();
     }
     
     public static List<Intervention> findInterventionsByDay(Date day) {
-        Query query = JpaUtil.getEntityManager().createQuery("SELECT i FROM Intervention i WHERE i.startDate = :day")
+        Query query = JpaUtil.getEntityManager().createQuery("SELECT i FROM Intervention i WHERE i.startDate = :day ORDER BY i.startDate")
                 .setParameter("day", day, TemporalType.DATE);
         return (List<Intervention>) query.getResultList();
     }
