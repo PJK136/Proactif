6ed14872a2f1c434b46d6ac91c2460a553b702c7
Paul Du <du.paul136@gmail.com>
2018-03-23 02:20:12 +0100

    Fin de l'IHM

--- src/main/java/test/Scenario.java
+++ src/main/java/test/Scenario.java
@@ -9,10 +9,13 @@ import entities.InterventionAnimal;
 import entities.InterventionIncident;
 import entities.InterventionLivraison;
 import entities.Person;
+import java.nio.file.Paths;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 import services.Service;
 import services.util.ConsoleIO;
+import services.util.CsvConvert;
 
 /**
  *
@@ -44,7 +47,7 @@ public class Scenario {
     private static class Accueil extends Action {
         @Override
         public Action run() {
-            return showMenu("Proact'IF", new Action[] {new Inscription(), new Connexion(), new Quitter()});
+            return showMenu("Proact'IF", new Action[] {new Inscription(), new Connexion(), new GodMode(), new Quitter()});
         }
     }
     
@@ -136,7 +139,7 @@ public class Scenario {
                 return new AccueilClient((Client) person);
             } else {
                 ConsoleIO.println("Bonjour " + person.getFirstName() + ".");
-                return null;
+                return new AccueilEmploye((Employee) person);
             }
         }
     }
@@ -157,7 +160,7 @@ public class Scenario {
         public Action run() {
             return showMenu(getName(), new Action[] {
                 new DemandeIntervention(client),
-                new ListeInterventions(client),
+                new ListeInterventionsClient(client),
                 new Accueil() {
                     @Override
                     public String getName() {
@@ -234,10 +237,10 @@ public class Scenario {
         }
     }
     
-    private static class ListeInterventions extends Action {
+    private static class ListeInterventionsClient extends Action {
         private final Client client;
         
-        public ListeInterventions(Client client) {
+        public ListeInterventionsClient(Client client) {
             this.client = client;
         }
         
@@ -253,9 +256,216 @@ public class Scenario {
                 ConsoleIO.println(intervention.toString());
             }
             
+            if (interventions.isEmpty()) {
+                ConsoleIO.println("Vous n'avez pas encore effectué de demandes d'intervention.");
+            }
+            
             return new AccueilClient(client);
         }
         
+    }
+    
+    private static class AccueilEmploye extends Action {
+        private Employee employee;
+        
+        public AccueilEmploye(Employee employee) {
+            this.employee = employee;
+        }
+        
+        @Override
+        public String getName() {
+            return "Accueil Employé";
+        }
+        
+        @Override
+        public Action run() {
+            return showMenu(getName(), new Action[] {
+                new InterventionEnCours(employee),
+                new FinirInterventionEnCours(employee),
+                new ListeInterventionsFiniesEmploye(employee),
+                new ListeInterventionsDuJour(employee),
+                new Accueil() {
+                    @Override
+                    public String getName() {
+                        return "Déconnexion";
+                    }
+                }
+            });
+        }
+    }
+    
+    private static class InterventionEnCours extends Action {
+        private Employee employee;
+        
+        public InterventionEnCours(Employee employee) {
+            this.employee = employee;
+        }
+        
+        @Override
+        public String getName() {
+            return "Intervention en cours";
+        }
+        
+        @Override
+        public Action run() {
+            Intervention intervention = Service.getInterventionToDoByEmployee(employee.getId());
+            if (intervention == null) {
+                ConsoleIO.println("Vous n'avez pas d'intervention en cours.");
+            } else {
+                ConsoleIO.println(intervention.toString());
+            }
+            
+            return new AccueilEmploye(employee);
+        }
+    }
+    
+    private static class FinirInterventionEnCours extends Action {
+        private Employee employee;
+        
+        public FinirInterventionEnCours(Employee employee) {
+            this.employee = employee;
+        }
+        
+        @Override
+        public String getName() {
+            return "Finir l'intervention en cours";
+        }
+        
+        @Override
+        public Action run() {
+            Intervention intervention = Service.getInterventionToDoByEmployee(employee.getId());
+            if (intervention == null) {
+                ConsoleIO.println("Vous n'avez pas d'intervention en cours.");
+            } else {
+                String success = ConsoleIO.ask("Succès de l'intervention ?").toLowerCase();
+                while (!success.equals("oui") && !success.equals("non")) {
+                    success = ConsoleIO.ask("Oui ou non ?").toLowerCase();
+                }
+                
+                intervention.setSuccess(success.equals("oui"));
+                intervention.setComment(ConsoleIO.ask("Commentaire :"));
+                if (Service.fillAttestation(intervention)) {
+                    ConsoleIO.println("Intervention terminée !");
+                } else {
+                    ConsoleIO.println("Impossible de valider l'intervention. Veuillez réssayer ultérieurement.");
+                }
+            }
+            
+            return new AccueilEmploye(employee);
+        }
+    }
+        
+    private static class ListeInterventionsFiniesEmploye extends Action {
+        private Employee employee;
+        
+        public ListeInterventionsFiniesEmploye(Employee employee) {
+            this.employee = employee;
+        }
+        
+        @Override
+        public String getName() {
+            return "Liste de mes interventions finies";
+        }
+        
+        @Override
+        public Action run() {
+            List<Intervention> interventions = Service.getFinishedInterventionsByEmployee(employee.getId());
+            for (Intervention intervention : interventions) {
+                ConsoleIO.println(intervention.toString());
+            }
+            
+            if (interventions.isEmpty()) {
+                ConsoleIO.println("Vous n'avez pas encore effectué d'intervention.");
+            }
+            
+            return new AccueilEmploye(employee);
+        }
+    }
+    
+    private static class ListeInterventionsDuJour extends Action {
+        private Employee employee;
+        
+        public ListeInterventionsDuJour(Employee employee) {
+            this.employee = employee;
+        }
+        
+        @Override
+        public String getName() {
+            return "Liste des interventions du jour";
+        }
+        
+        @Override
+        public Action run() {
+            List<Intervention> interventions = Service.getInterventionsByDay(new Date());
+            for (Intervention intervention : interventions) {
+                ConsoleIO.println(intervention.toString());
+            }
+            
+            if (interventions.isEmpty()) {
+                ConsoleIO.println("Vous n'avez pas encore effectué d'intervention.");
+            }
+            
+            return new AccueilEmploye(employee);
+        }
+    }
+    
+    private static class GodMode extends Action {
+        @Override
+        public String getName() {
+            return "God Mode";
+        }
+
+        @Override
+        public Action run() {
+            return showMenu(getName(), new Action[] {
+                new ChargementDonneesTest(),
+                new Accueil() {
+                    @Override
+                    public String getName() {
+                        return "Accueil";
+                    }
+                }
+            });
+        }
+    }
+    
+    private static class ChargementDonneesTest extends Action {
+        final static String DATA_PATH = "src/main/resources/mockData/";
+        final static String DEFAULT_PASSWORD = "password";
+
+        @Override
+        public String getName() {
+            return "Chargement des données de test";
+        }
+
+        @Override
+        public Action run() {
+            List<Client> clients = CsvConvert.loadClientsWithAddresses(Paths.get(DATA_PATH + "clients.csv"), Paths.get(DATA_PATH + "clients_addresses.csv"));
+            List<Employee> employees = CsvConvert.loadEmployeesWithAddresses(Paths.get(DATA_PATH + "employees.csv"), Paths.get(DATA_PATH + "employees_addresses.csv"));
+            
+            for (Employee employee : employees) {
+                if (employee.getAddress().getGeoCoords() == null) {
+                    continue;
+                }
+                Service.register(employee, DEFAULT_PASSWORD.toCharArray());
+            }
+            
+            int i = 0;
+            for (Client client : clients) {
+                if (client.getAddress().getGeoCoords() == null) {
+                    continue;
+                }
+                Service.register(client, DEFAULT_PASSWORD.toCharArray());
+                i++;
+                
+                if (i > 20) {
+                    break;
+                }
+            }
+            
+            ConsoleIO.println("Chargement terminé !");
+            return new GodMode();
+        }
     }
             
     private static class Quitter extends Action {
